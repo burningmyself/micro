8a9e17cbad7e2b62aff5e59e3586f2d29f4209fb
yangfubing <1928881525@qq.com>
2019-10-30 18:24:34 +0800

    update admin api gateway

--- gateways/AdminApiGateway.Host/AdminApiGatewayModule.cs
+++ gateways/AdminApiGateway.Host/AdminApiGatewayModule.cs
@@ -8,6 +8,10 @@ using Volo.Abp.Modularity;
 using Volo.Abp.Localization;
 using Base.UI;
 using Microsoft.OpenApi.Models;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System;
+using System.Linq;
 
 namespace AdminApiGateway.Host
 {
@@ -17,58 +21,41 @@ namespace AdminApiGateway.Host
         )]
     public class AdminApiGatewayModule : AbpModule
     {
+        // 默认的跨域请求策略名称
+        private const string _defaultCorsPolicyName = "Admin.Api.Cors";
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
             var configuration = context.Services.GetConfiguration();
 
-            context.Services.AddApiVersioning(option =>
-            {
-                option.AssumeDefaultVersionWhenUnspecified = true;
-                option.ReportApiVersions = false;
-            });
-           //.AddMvcCore().Services.AddVersionedApiExplorer(option =>
-           //{
-           //    option.GroupNameFormat = "'v'VVV";
-           //    option.AssumeDefaultVersionWhenUnspecified = true;
-           //});
+            context.Services.AddAuthentication("Bearer")
+                 .AddIdentityServerAuthentication(options =>
+                 {
+                     options.Authority = configuration["AuthServer:Authority"];
+                     options.ApiName = configuration["AuthServer:ApiName"];
+                     options.RequireHttpsMetadata = false;
+                    //TODO: Should create an extension method for that (may require to create a new ABP package depending on the IdentityServer4.AccessTokenValidation)
+                    //options.InboundJwtClaimTypeMap["sub"] = AbpClaimTypes.UserId;
+                    //options.InboundJwtClaimTypeMap["role"] = AbpClaimTypes.Role;
+                    //options.InboundJwtClaimTypeMap["email"] = AbpClaimTypes.Email;
+                    //options.InboundJwtClaimTypeMap["email_verified"] = AbpClaimTypes.EmailVerified;
+                    //options.InboundJwtClaimTypeMap["phone_number"] = AbpClaimTypes.PhoneNumber;
+                    //options.InboundJwtClaimTypeMap["phone_number_verified"] = AbpClaimTypes.PhoneNumberVerified;
+                    //options.InboundJwtClaimTypeMap["name"] = AbpClaimTypes.UserName;
+                });
+
             context.Services.AddSwaggerGen(options =>
             {
-                options.SwaggerDoc("v1", new OpenApiInfo { Title = "AdminApiGateway API", Version = "v1" });
-                //options.SwaggerDoc("v2", new Info { Title = "App API", Version = "v2" });
-
-                options.DocInclusionPredicate((docName, description) =>
-                {
-                    if (description.RelativePath.Contains("api-version"))
-                    {
-                        return false;
-                    }
-                    //if ("v1" == docName && description.RelativePath.Split(@"/")[0] != "App")
-                    //{
-                    //    return true;
-                    //}
-                    //if ("v2" == docName && description.RelativePath.Split(@"/")[0] == "App")
-                    //{
-                    //    return true;
-                    //}
-                    return true;
-                });
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "AdminApiGateway Gateway API", Version = "v1" });
+                options.DocInclusionPredicate((docName, description) => true);
                 options.CustomSchemaIds(type => type.FullName);
-
-                //var security = new Dictionary<string, IEnumerable<string>> { { "Bearer", new string[] { } }, };
-                //options.AddSecurityRequirement(security);//添加一个必须的全局安全信息，和AddSecurityDefinition方法指定的方案名称要一致，这里是Bearer。
-                //options.AddSecurityDefinition("Bearer", new ApiKeyScheme
-                //{
-                //    Description = "JWT授权(数据将在请求头中进行传输) 参数结构: \"Authorization: Bearer {token}\"",
-                //    Name = "Authorization",//jwt默认的参数名称
-                //    In = "header",//jwt默认存放Authorization信息的位置(请求头中)
-                //    Type = "apiKey"
-                //});
             });
+            context.Services.AddOcelot(context.Services.GetConfiguration());
 
-            context.Services.AddDistributedRedisCache(options =>
-            {
-                options.Configuration = configuration["Redis:Configuration"];
-            });
+            //Configure<AbpDbContextOptions>(options =>
+            //{
+            //    options.UseSqlServer();
+            //});
+
             Configure<AbpLocalizationOptions>(options =>
             {
                 options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "中文", flagIcon: "ios-add"));
@@ -76,51 +63,49 @@ namespace AdminApiGateway.Host
                 //...add other languages
             });
 
+            context.Services.AddDistributedRedisCache(options =>
+            {
+                options.Configuration = configuration["Redis:Configuration"];
+            });           
 
             var redis = ConnectionMultiplexer.Connect(configuration["Redis:Configuration"]);
             context.Services.AddDataProtection()
                 .PersistKeysToStackExchangeRedis(redis, "Ms-DataProtection-Keys");
 
-
+            #region "配置 CORS 授权策略"
+            context.Services.AddCors(options => options.AddPolicy(_defaultCorsPolicyName,
+            builder => builder.WithOrigins(
+                    configuration["AllowedHosts"]
+                    .Split(",", StringSplitOptions.RemoveEmptyEntries).ToArray()
+                )
+            .AllowAnyHeader()
+            .AllowAnyMethod()));
+            #endregion
         }
 
         public override void OnApplicationInitialization(ApplicationInitializationContext context)
         {
             var app = context.GetApplicationBuilder();
-
             app.UseCorrelationId();
             app.UseVirtualFiles();
+            app.UseRouting();
             app.UseAuthentication();
-            app.UseAbpRequestLocalization();
-
-            app.UseCors(builder => builder
-                .WithOrigins("http://localhost:9000")
-                .WithOrigins("http://localhost:9527")
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .AllowCredentials());
-
-            //var provider = app.ApplicationServices.GetRequiredService<IApiVersionDescriptionProvider>();
-
-            //app.UseSwagger();
-            //app.UseSwaggerUI(c =>
-            //{
-            //    foreach (var description in provider.ApiVersionDescriptions)
-            //    {
-            //        c.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
-            //    }
-            //});
+            // 允许跨域请求访问
+            app.UseCors(_defaultCorsPolicyName);
             app.UseSwagger();
             app.UseSwaggerUI(options =>
             {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "AdminApiGateway API");
-                //options.SwaggerEndpoint("/swagger/v2/swagger.json", "App API");
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "AdminApiGateway Gateway API");
             });
+
             //app.MapWhen(
             //    ctx => ctx.Request.Path.ToString().StartsWith("/api/abp/") ||
             //           ctx.Request.Path.ToString().StartsWith("/Abp/"),
             //    app2 => { app2.UseMvcWithDefaultRouteAndArea(); }
-            //);           
+            //);
+
+            app.UseOcelot().Wait();
+
         }
     }
 }
